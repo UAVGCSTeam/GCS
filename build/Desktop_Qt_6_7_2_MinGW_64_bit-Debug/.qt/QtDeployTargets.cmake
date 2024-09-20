dc72f30912bb607ffdefdc3ac1aac9665f420f3f
Mbee0 <mbee@cpp.edu>
2024-09-20 14:53:30 -0700

    first commit

---
+++ build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_UAVGCS_FILE C:/Users/megan/OneDrive/Documents/UAVGCS/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/UAVGCS.exe)
+set(__QT_DEPLOY_TARGET_UAVGCS_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_UAVGCS_RUNTIME_DLLS C:/Qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
