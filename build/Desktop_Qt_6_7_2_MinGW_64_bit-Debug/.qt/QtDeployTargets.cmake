6736be973150ea54d2f5ee02fa5e4a0ab2dc7925
Brandon Shippy <0brandonshippy@gmail.com>
2024-09-25 19:41:16 -0700

    Applied .gitignore

--- build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_GCS_FILE C:/Users/Brandon/Desktop/Programming/GCS/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/GCS.exe)
-set(__QT_DEPLOY_TARGET_GCS_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_GCS_RUNTIME_DLLS C:/Qt/6.7.2/mingw_64/bin/Qt6QuickWidgets.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Quick.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6QmlModels.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6OpenGL.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Qml.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Network.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
