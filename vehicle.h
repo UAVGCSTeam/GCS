4dc61112015175d404567114cf43dc87626302c9
ML Laptop <giandmarquez@gmail.com>
2025-01-26 17:29:32 -0800

    rewrote c++ code, check for error

--- vehicle.h
+++ vehicle.h
@@ -6,63 +6,150 @@
 // Not using namespace std, just for better C++ mem protection
 // Seperating pybind and Vehicle Class for now
 
-// TODO: Check what needs to be constant, so values aren't changing
-class Vehicle
-{
-    //Q_OBJECT
+// TODO: Check what needs to be constant, so values aren’t changing.
+/**
+ * @class Vehicle
+ * @brief Represents a drone vehicle with properties for identification, 
+ *        location, and status tracking.
+ */
+class Vehicle {
 public:
-    Vehicle(); // Default constructor
-    Vehicle(std::string input_id, std::string input_name, std::string input_role, std::string input_remoteXbeeAddress);
-    ~Vehicle(); // Deconstructor, delete Drone
-    // Moved Vector List for Vehicles to another class, Vehicle Manager...
+    /**
+     * Default constructor.
+     * Initializes a Vehicle object with default values.
+     */
+    Vehicle();
 
+    /**
+     * Parameterized constructor.
+     * Initializes a Vehicle object with specific values.
+     * @param input_id The unique ICAO ID of the vehicle.
+     * @param input_name The name assigned to the vehicle.
+     * @param input_role The role or purpose of the vehicle.
+     * @param input_remoteXbeeAddress The Xbee address for remote connection.
+     */
+    Vehicle(const std::string &input_id, const std::string &input_name, const std::string &input_role, const std::string &input_remoteXbeeAddress);
+    // Moved Vector List for Vehicles to another class, Vehicle Manager...
+    /**
+     * Destructor.
+     * Cleans up any resources used by the Vehicle object.
+     */
+    ~Vehicle();
+
+    /**
+     * Checks if the Xbee module is connected.
+     * @return True if connected, false otherwise.
+     */
     bool checkIfXbeeConnected();
+
     // Setters and Getters
-    // got rid of getVehicle
-    void updateID(std::string inputId) { ICAO_ID = inputId; }
+    /**
+     * Updates the ICAO ID of the vehicle.
+     * @param inputId New ID for the vehicle.
+     */
+    void updateID(const std::string &inputId) { ICAO_ID = inputId; }
+
+    /**
+     * Gets the ICAO ID of the vehicle.
+     * @return The ICAO ID as a string.
+     */
     std::string getID() const { return ICAO_ID; }
 
-    // I'm having a brain fart on how vehicle goes, I guess if you call Vehicle(A).updateRole, it wouldn't need an ID?
-    void updateRole(std::string inputID, std::string inputRole);
     /**
-     *  Old role, might need this->?
+     * Updates the role of the vehicle.
+     * @param inputRole New role assigned to the vehicle.
      */
-    void updateRole(std::string inputRole) { role = inputRole; };
-    std::string getRole() {return role; }
+    void updateRole(const std::string &inputRole) { role = inputRole; }
 
-    void updateStatus(std::string inputStatus) { status = inputStatus; }
-    std::string getStatus() { return status; }
-
-    void updateBatteryLevel(double inputBatteryLevel) { batteryLevel = inputBatteryLevel; }
-    double getBatteryLevel() { return batteryLevel; }
-
-    void updatePosition(float longitude, float latitude, float altitude);
-    float *getPosition() { return position; }
-
-    // changed int to float -- havent checked if ths breaks python stuff
-    void updateVelocity(float x, float y, float z);
-    int *getVelocity() { return velocity; }
-
-    void updateOrientation(float x, float y, float z);
-    int *getOrientation() { return orientation; }
-
-    void printAllDroneInfo();
-private:
-    std::string ICAO_ID;   // ICAO currently labeled as "A" [Xbee]
-    std::string name;      // "Death Hawk"
-    std::string role;      // "Supression, Detection" ->
     /**
-     * TODO: Move Role as Lead to other classes in future? where parent Drone -> Suppression, Detection.h
-     * Hell we can make it so it's only a ID and name with other functions like type of connection -> xbee, etc...
-    */
-    std::string status;    // Not sure actually
-    std::string remoteXbeeAddress;
-    // Changed int to Double
-    double batteryLevel = -1; // Default as -1, to show it's not actually recieving battery
-    // It's an array because Megan wants everything in a tuple...?
-    float position[3] = {-1, -1, -1};  // longitude, lattidue, altitude
-    int velocity[3] = {-1, -1, -1};    // x, y, z
-    int orientation[3] = {-1, -1, -1}; // x, y, z
+     * Gets the role of the vehicle.
+     * @return The role as a string.
+     */
+    std::string getRole() const { return role; }
+
+    /**
+     * Updates the current status of the vehicle.
+     * @param inputStatus New status of the vehicle.
+     */
+    void updateStatus(const std::string &inputStatus) { status = inputStatus; }
+
+    /**
+     * Gets the current status of the vehicle.
+     * @return The status as a string.
+     */
+    std::string getStatus() const { return status; }
+
+    /**
+     * Updates the battery level of the vehicle.
+     * @param inputBatteryLevel New battery level percentage.
+     */
+    void updateBatteryLevel(double inputBatteryLevel) { batteryLevel = inputBatteryLevel; }
+
+    /**
+     * Gets the current battery level of the vehicle.
+     * @return The battery level as a double.
+     */
+    double getBatteryLevel() const { return batteryLevel; }
+
+    /**
+     * Updates the position of the vehicle.
+     * @param longitude New longitude position.
+     * @param latitude New latitude position.
+     * @param altitude New altitude level.
+     */
+    void updatePosition(float longitude, float latitude, float altitude);
+
+    /**
+     * Gets the position of the vehicle.
+     * @return A pointer to an array containing longitude, latitude, and altitude.
+     */
+    const float* getPosition() const { return position; }
+
+    /**
+     * Updates the velocity of the vehicle.
+     * @param x Velocity along the x-axis.
+     * @param y Velocity along the y-axis.
+     * @param z Velocity along the z-axis.
+     */
+    void updateVelocity(float x, float y, float z);
+
+    /**
+     * Gets the velocity of the vehicle.
+     * @return A pointer to an array containing x, y, and z velocity components.
+     */
+    const float* getVelocity() const { return velocity; }
+
+    /**
+     * Updates the orientation of the vehicle.
+     * @param x Orientation along the x-axis.
+     * @param y Orientation along the y-axis.
+     * @param z Orientation along the z-axis.
+     */
+    void updateOrientation(float x, float y, float z);
+
+    /**
+     * Gets the orientation of the vehicle.
+     * @return A pointer to an array containing x, y, and z orientation components.
+     */
+    const int* getOrientation() const { return orientation; }
+
+    /**
+     * Prints all information related to the vehicle for debugging or display.
+     */
+    void printAllDroneInfo() const;
+
+private:
+    std::string ICAO_ID;        /** Unique ID for the vehicle (ICAO ID). */
+    std::string name;           /** Name of the vehicle. */
+    std::string role;           /** Role or mission type of the vehicle. */
+    std::string status;         /** Current status (offline, connected, armed, flying). */
+    // TODO: For now I think status as string makes sense, but functionally I'm imagining a switch statement as a number should be shorter byte
+
+    std::string remoteXbeeAddress; /** Address for remote Xbee connectivity. */
+    double batteryLevel = -1;   /** Battery level, default -1 indicates no data. */
+    float position[3] = {-1, -1, -1}; /** Position array: longitude, latitude, altitude. */
+    float velocity[3] = {-1, -1, -1};   /** Velocity array: x, y, z components. */
+    int orientation[3] = {-1, -1, -1}; /** Orientation array: x, y, z components. */
 };
 
 #endif // VEHICLE_H
