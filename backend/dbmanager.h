132b00bdd6b373941fd2c39136009b97527433e4
cmccarry <ctmccarry@gmail.com>
2025-03-01 17:58:18 -0800

    update droneList from database
    - added declarations to header files and functions to source files to be able to pull from db tables
    - can be tested by uncommenting out the code lines in the fetch function in main.qml
    - pointing out name type in droneController and droneclass.h
    - todo: when adding a drone it adds it to the db table but doesnt update the drone list view in the application. the drone list is only populated on startup based on the existing table in sqlite

--- backend/dbmanager.h
+++ backend/dbmanager.h
@@ -51,7 +51,10 @@ public:
     bool editDrone(int droneID, const QString& droneName = QString(), const QString& droneType = QString(),
                    const QString& xbeeID = QString(), const QString& xbeeAddress = QString());
     bool deleteDrone(int droneID);
-    void printDroneList(); // essentially Reading the drone, has some basecode 
+    void printDroneList(); // essentially Reading the drone, has some basecode
+
+    // Declaration to fetch all drone records from the database
+    QList<QVariantMap> fetchAllDrones();
 
 private:
     QSqlDatabase gcs_db_connection;
